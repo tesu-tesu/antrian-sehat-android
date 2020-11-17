b1cbfbc55af8bed5833079ea8da93b30eda39309
satriyoaji <satriyoaji27@gmail.com>
2020-11-18 02:27:21 +0700

    Login Logic & redirect to home ( on progress )

---
+++ app/src/main/java/com/antriansehat/application/view/HomeActivity.java
@@ -0,0 +1,38 @@
+package com.antriansehat.application.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.antriansehat.application.R;
+import com.antriansehat.application.contract.HomeContract;
+import com.antriansehat.application.interactor.HomeInteractor;
+import com.antriansehat.application.presenter.HomePresenter;
+import com.antriansehat.application.util.UtilProvider;
+
+public class HomeActivity extends AppCompatActivity implements HomeContract.View {
+    private HomeContract.Presenter presenter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        presenter = new HomePresenter(this, new HomeInteractor(UtilProvider.getSharedPreferencesUtil()));
+        presenter.checkIsUserLogin();
+    }
+
+    @Override
+    public void whenUserLogin() {
+        finish();
+//        startActivity(new Intent(getApplicationContext(), ListBookActivity.class));
+        //loading data from API to show in home
+    }
+
+    @Override
+    public void whenUserNotLogin() {
+        finish();
+        startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+    }
+}
