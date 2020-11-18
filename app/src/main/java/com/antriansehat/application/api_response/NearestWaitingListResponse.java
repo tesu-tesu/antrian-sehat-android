e6c8780cde464a8546310b42e5f6e70352b84ede
Rochimatus Sa'diyah <45308962+rochimatus@users.noreply.github.com>
2020-11-18 22:41:42 +0700

    Revert "progress activity home & add gitignore"

--- app/src/main/java/com/antriansehat/application/api_response/NearestWaitingListResponse.java
+++
@@ -1,9 +0,0 @@
-package com.antriansehat.application.api_response;
-
-import com.antriansehat.application.model.WaitingList;
-
-public class NearestWaitingListResponse {
-    public boolean success;
-    public boolean message;
-    public WaitingList waitingList;
-}
