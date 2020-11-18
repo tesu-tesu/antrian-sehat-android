e8ba08a49e284f5cc1121481649ccad59d30efa8
rochimatus <rochimatus22@gmail.com>
2020-11-18 20:35:22 +0700

    progress activity home 40%

---
+++ app/src/main/java/com/antriansehat/application/api_response/NearestWaitingListResponse.java
@@ -0,0 +1,9 @@
+package com.antriansehat.application.api_response;
+
+import com.antriansehat.application.model.WaitingList;
+
+public class NearestWaitingListResponse {
+    public boolean success;
+    public boolean message;
+    public WaitingList waitingList;
+}
--- app/src/main/java/com/antriansehat/application/view/HomeActivity.java
+++ app/src/main/java/com/antriansehat/application/view/HomeActivity.java
@@ -1,31 +1,58 @@
 package com.antriansehat.application.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.antriansehat.application.R;
 import com.antriansehat.application.contract.HomeContract;
+import com.antriansehat.application.databinding.ActivityHomeBinding;
+import com.antriansehat.application.databinding.ActivityListBookBinding;
 import com.antriansehat.application.interactor.HomeInteractor;
+import com.antriansehat.application.interactor.ListBookInteractor;
+import com.antriansehat.application.model.Article;
+import com.antriansehat.application.model.WaitingList;
 import com.antriansehat.application.presenter.HomePresenter;
+import com.antriansehat.application.presenter.ListBookPresenter;
 import com.antriansehat.application.util.UtilProvider;
 
-public class HomeActivity extends AppCompatActivity implements HomeContract.View {
+import java.util.List;
+
+public class HomeActivity extends AppCompatActivity implements HomeContract.View, View.OnClickListener {
+    private ActivityHomeBinding binding;
     private HomeContract.Presenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         presenter = new HomePresenter(this, new HomeInteractor(UtilProvider.getSharedPreferencesUtil()));
+
+        initView();
         presenter.checkIsUserLogin();
+        presenter.requestNearestWaitingList();
+    }
+
+    private void initView() {
+        binding.btCreateWL.setOnClickListener(this);
+        binding.btListHA.setOnClickListener(this);
+        binding.btListPoly.setOnClickListener(this);
+        binding.btShowTicket.setOnClickListener(this);
+        binding.cNearestWaitingList.setOnClickListener(this);
+
+/*        binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        binding.logoutButton.setOnClickListener(this);*/
     }
 
     @Override
     public void whenUserLogin() {
-        finish();
+        presenter.requestNearestWaitingList();
+//        finish();
 //        startActivity(new Intent(getApplicationContext(), ListBookActivity.class));
         //loading data from API to show in home
     }
@@ -34,5 +61,55 @@ public class HomeActivity extends AppCompatActivity implements HomeContract.View
     public void whenUserNotLogin() {
         finish();
         startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+    }
+
+    @Override
+    public void showNearestWaitingList(WaitingList waitingList) {
+
+    }
+
+    @Override
+    public void showListArticle(List<Article> articles) {
+
+    }
+
+    @Override
+    public void showError(String errorMessage) {
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == binding.cNearestWaitingList.getId()){
+            onCardNearestWaitingList();
+        }else if(view.getId() == binding.btShowTicket.getId()){
+            onButtonShowTicket();
+        }else if(view.getId() == binding.btListPoly.getId()){
+            onButtonListPoly();
+        }else if(view.getId() == binding.btListHA.getId()){
+            onButtonListHA();
+        }else if(view.getId() == binding.btCreateWL.getId()){
+            onButtonCreateWaitingList();
+        }
+    }
+
+    public void onCardNearestWaitingList() {
+
+    }
+
+    public void onButtonShowTicket() {
+
+    }
+
+    public void onButtonListPoly() {
+
+    }
+
+    public void onButtonListHA() {
+
+    }
+
+    public void onButtonCreateWaitingList() {
+
     }
 }
