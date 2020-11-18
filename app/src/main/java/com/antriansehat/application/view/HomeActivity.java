74df6a683f71def7d7b881e15604a1302a4a632b
satriyoaji <satriyoaji27@gmail.com>
2020-11-19 03:17:03 +0700

    connecting bottom bar to selected layout

--- app/src/main/java/com/antriansehat/application/view/HomeActivity.java
+++ app/src/main/java/com/antriansehat/application/view/HomeActivity.java
@@ -1,26 +1,38 @@
 package com.antriansehat.application.view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.antriansehat.application.R;
 import com.antriansehat.application.contract.HomeContract;
+import com.antriansehat.application.databinding.ActivityHomeBinding;
 import com.antriansehat.application.interactor.HomeInteractor;
 import com.antriansehat.application.presenter.HomePresenter;
 import com.antriansehat.application.util.UtilProvider;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class HomeActivity extends AppCompatActivity implements HomeContract.View {
+public class HomeActivity extends AppCompatActivity implements HomeContract.View, BottomNavigationView.OnNavigationItemReselectedListener {
     private HomeContract.Presenter presenter;
+    private ActivityHomeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         presenter = new HomePresenter(this, new HomeInteractor(UtilProvider.getSharedPreferencesUtil()));
         presenter.checkIsUserLogin();
+
+        initView();
+    }
+
+    private void initView() {
+        binding.bottomNav.setOnNavigationItemReselectedListener(this);
     }
 
     @Override
@@ -32,5 +44,21 @@ public class HomeActivity extends AppCompatActivity implements HomeContract.View
     public void whenUserNotLogin() {
         finish();
         startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+    }
+
+    @Override
+    public void onNavigationItemReselected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_home:
+                break;
+            case R.id.action_user:
+                Intent profile = new Intent(HomeActivity.this,ProfileActivity.class);
+                startActivity(profile);
+                break;
+            case R.id.action_time:
+                Intent time = new Intent(HomeActivity.this,RiwayatTiketActivity.class);
+                startActivity(time);
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/antriansehat/application/view/ProfileActivity.java
@@ -0,0 +1,16 @@
+package com.antriansehat.application.view;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.antriansehat.application.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_page);
+    }
+}
