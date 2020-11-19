1f7e1ab2a5dc24bd84ac60e5cf3c90c6c423ea8e
rochimatus <rochimatus22@gmail.com>
2020-11-19 09:26:54 +0700

    activity home works

--- app/src/main/java/com/antriansehat/application/view/HomeActivity.java
+++ app/src/main/java/com/antriansehat/application/view/HomeActivity.java
@@ -2,35 +2,127 @@ package com.antriansehat.application.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.antriansehat.application.R;
 import com.antriansehat.application.contract.HomeContract;
+import com.antriansehat.application.databinding.ActivityHomeBinding;
 import com.antriansehat.application.interactor.HomeInteractor;
+import com.antriansehat.application.model.Article;
+import com.antriansehat.application.model.WaitingList;
 import com.antriansehat.application.presenter.HomePresenter;
 import com.antriansehat.application.util.UtilProvider;
 
-public class HomeActivity extends AppCompatActivity implements HomeContract.View {
+import java.util.List;
+
+import static androidx.constraintlayout.widget.ConstraintSet.VISIBLE;
+
+public class HomeActivity extends AppCompatActivity implements HomeContract.View, View.OnClickListener {
     private HomeContract.Presenter presenter;
+    private ActivityHomeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         presenter = new HomePresenter(this, new HomeInteractor(UtilProvider.getSharedPreferencesUtil()));
+
+        initView();
         presenter.checkIsUserLogin();
+        presenter.requestNearestWaitingList();
+    }
+
+    private void initView() {
+        binding.btCreateWL.setOnClickListener(this);
+        binding.btListHA.setOnClickListener(this);
+        binding.btListPoly.setOnClickListener(this);
+        binding.btShowTicket.setOnClickListener(this);
+        binding.cWaitingList.setOnClickListener(this);
+
+/*        binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        binding.logoutButton.setOnClickListener(this);*/
     }
 
     @Override
     public void whenUserLogin() {
+        presenter.requestNearestWaitingList();
+//        finish();
+//        startActivity(new Intent(getApplicationContext(), ListBookActivity.class));
         //loading data from API to show in home
     }
 
-    @Override
     public void whenUserNotLogin() {
         finish();
         startActivity(new Intent(getApplicationContext(), LoginActivity.class));
     }
+
+    @Override
+    public void showNearestWaitingList(WaitingList waitingList) {
+        binding.errMessage.setText("");
+        binding.currentWaitingList.setText(waitingList.getCurrent_number());
+        binding.latestWaitingList.setText(waitingList.getLatest_number());
+        binding.orderNumber.setText(waitingList.getOrder_number());
+        binding.tvResidenceNumber.setText(waitingList.getResidence_number());
+        binding.tvPoly.setText(waitingList.getPolyclinic());
+        binding.tvHA.setText(waitingList.getHealth_agency());
+        binding.tvRegDate.setText(waitingList.getRegistered_date().toString());
+    }
+
+    @Override
+    public void showListArticle(List<Article> articles) {
+        //
+    }
+
+    @SuppressLint("WrongConstant")
+    @Override
+    public void showError(String errorMessage) {
+        binding.currentWaitingListLabel.setText("");
+        binding.orderNumberLabel.setText("");
+        binding.waitingListLabel.setText("");
+        binding.errMessage.setText(errorMessage);
+        binding.errMessage.setVisibility(VISIBLE);
+        Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == binding.cWaitingList.getId()){
+            onCardNearestWaitingList();
+        }else if(view.getId() == binding.btShowTicket.getId()){
+            onButtonShowTicket();
+        }else if(view.getId() == binding.btListPoly.getId()){
+            onButtonListPoly();
+        }else if(view.getId() == binding.btListHA.getId()){
+            onButtonListHA();
+        }else if(view.getId() == binding.btCreateWL.getId()){
+            onButtonCreateWaitingList();
+        }
+    }
+
+    public void onCardNearestWaitingList() {
+        //tampilin fragment ticket dengan barcode
+    }
+
+    public void onButtonShowTicket() {
+        //pindah ke show ticket activity
+    }
+
+    public void onButtonListPoly() {
+        //pindah ke list poly activity
+    }
+
+    public void onButtonListHA() {
+        //pindah ke health agency activity
+    }
+
+    public void onButtonCreateWaitingList() {
+        //pindah ke health agency activity
+    }
+
 }
