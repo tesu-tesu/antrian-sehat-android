85150371547e929aea48c4f1b01a0fe34eb2eea5
Yanu Adi Nugraha <2110181004@msoffice.pens.ac.id>
2020-11-20 17:33:38 +0700

    list puskesmas 100%

--- app/src/main/java/com/antriansehat/application/api_response/ListHealthAgencyResponse.java
+++ app/src/main/java/com/antriansehat/application/api_response/ListHealthAgencyResponse.java
@@ -1,11 +1,9 @@
 package com.antriansehat.application.api_response;
 
-import com.antriansehat.application.model.HealthAgency;
-
-import java.util.List;
+import com.antriansehat.application.model.Pagination;
 
 public class ListHealthAgencyResponse {
     public boolean success;
     public String message;
-    public List<HealthAgency> data;
+    public Pagination data;
 }
--- app/src/main/java/com/antriansehat/application/model/HealthAgency.java
+++ app/src/main/java/com/antriansehat/application/model/HealthAgency.java
@@ -1,33 +1,63 @@
 package com.antriansehat.application.model;
 
-import android.media.Image;
-
 public class HealthAgency {
     private String id;
-    private String name;
-    private String address;
-    private Image image;
+    private String name, address, image, call_center, email;
 
-    public HealthAgency(String id, String name, String address, Image image) {
+    public HealthAgency(String id, String name, String address, String image, String call_center, String email) {
         this.id = id;
         this.name = name;
         this.address = address;
         this.image = image;
+        this.call_center = call_center;
+        this.email = email;
     }
 
     public String getId() {
         return id;
     }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getAddress() {
         return address;
     }
 
-    public Image getImage() {
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getImage() {
         return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getCall_center() {
+        return call_center;
+    }
+
+    public void setCall_center(String call_center) {
+        this.call_center = call_center;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
