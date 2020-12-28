96d041ddbe94c8f4f5529e324b01ea87bf729526
satriyoaji <satriyoaji27@gmail.com>
2020-12-28 16:45:23 +0700

    restrict un-pasien user to login the app

--- app/src/main/java/com/antriansehat/application/api_response/LoginResponse.java
+++ app/src/main/java/com/antriansehat/application/api_response/LoginResponse.java
@@ -1,7 +1,10 @@
 package com.antriansehat.application.api_response;
 
+import com.antriansehat.application.model.User;
+
 public class LoginResponse {
     public boolean success;
     public String access_token;
     public String message;
+    public User user; //just for validation role when login
 }
