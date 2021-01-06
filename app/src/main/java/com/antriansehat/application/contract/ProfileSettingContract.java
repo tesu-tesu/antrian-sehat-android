7ad049647fc9b97b77867434bfdb015fd858cdb7
rochimatus <rochimatus22@gmail.com>
2021-01-06 20:08:59 +0700

    update profile image done

--- app/src/main/java/com/antriansehat/application/contract/ProfileSettingContract.java
+++ app/src/main/java/com/antriansehat/application/contract/ProfileSettingContract.java
@@ -5,6 +5,8 @@ import com.antriansehat.application.api_response.RegisterResponse;
 import com.antriansehat.application.callback.RequestCallback;
 import com.antriansehat.application.model.User;
 
+import java.io.File;
+
 public interface ProfileSettingContract {
     interface View {
         void startLoading();
@@ -13,15 +15,18 @@ public interface ProfileSettingContract {
         void updateSuccess(String message);
         void updateFailed(String message);
         void showUserData(User user);
+        void updateProfileImageSuccess(String message, String path);
     }
 
     interface Presenter {
         void setUserData();
         void updateProfile(String name, String email, String nik, String phone);
+        void updateProfileImage(File imageFile);
     }
 
     interface Interactor {
         void setDataUser(final RequestCallback<User> requestCallback);
         void requestUpdateProfile(String id, String name, String email, String nik, String phone, RequestCallback<User> requestCallback);
+        void updateProfileImage(String id, File imageFile, final RequestCallback<String> requestCallback);
     }
 }
--- app/src/main/java/com/antriansehat/application/view/ProfileSettingActivity.java
+++ app/src/main/java/com/antriansehat/application/view/ProfileSettingActivity.java
@@ -1,21 +1,31 @@
 package com.antriansehat.application.view;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.antriansehat.application.constant.ApiConstant;
 import com.antriansehat.application.contract.ProfileSettingContract;
 import com.antriansehat.application.databinding.ActivityProfileSettingBinding;
 import com.antriansehat.application.interactor.ProfileSettingInteractor;
 import com.antriansehat.application.model.User;
 import com.antriansehat.application.presenter.ProfileSettingPresenter;
 import com.antriansehat.application.util.UtilProvider;
+import com.github.dhaval2404.imagepicker.ImagePicker;
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
 
 public class  ProfileSettingActivity extends AppCompatActivity implements ProfileSettingContract.View, View.OnClickListener{
     private ProfileSettingPresenter presenter;
     private ActivityProfileSettingBinding binding;
+    private File file;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +42,7 @@ public class  ProfileSettingActivity extends AppCompatActivity implements Profil
     private void initView() {
         binding.btnSave.setOnClickListener(this);
         binding.btnCancel.setOnClickListener(this);
+        binding.btChangeProfileImage.setOnClickListener(this);
     }
 
     @Override
@@ -39,8 +50,33 @@ public class  ProfileSettingActivity extends AppCompatActivity implements Profil
         if(v.getId() == binding.btnSave.getId()){
             onSaveUpdateClick();
             backToProfile();
-        }else if(v.getId() == binding.btnCancel.getId()){
+        } else if(v.getId() == binding.btnCancel.getId()){
             backToProfile();
+        } else if(v.getId() == binding.btChangeProfileImage.getId()) {
+            selectImage();
+        }
+    }
+
+    private void selectImage() {
+        ImagePicker.Companion.with(this)
+                .cropSquare()  			//Crop image(Optional), Check Customization for more option
+                .compress(1024)			//Final image size will be less than 1 MB(Optional)
+                .maxResultSize(1080, 1080)	//Final image resolution will be less than 1080 x 1080(Optional)
+                .start();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == Activity.RESULT_OK){
+            Uri uri = data.getData();
+            binding.ivProfileImage.setImageURI(uri);
+            file = ImagePicker.Companion.getFile(data);
+            presenter.updateProfileImage(file);
+        }else if (resultCode == ImagePicker.RESULT_ERROR) {
+            Toast.makeText(this, ImagePicker.Companion.getError(data), Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Task Cancelled", Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -61,6 +97,7 @@ public class  ProfileSettingActivity extends AppCompatActivity implements Profil
     @Override
     public void onSaveUpdateClick() {
         binding.btnSave.setEnabled(false);
+
         presenter.updateProfile(binding.etNama.getText().toString(),
                 binding.etEmail.getText().toString(),
                 binding.etNIK.getText().toString(),
@@ -88,6 +125,21 @@ public class  ProfileSettingActivity extends AppCompatActivity implements Profil
         binding.etEmail.setText(user.getEmail());
         binding.etNIK.setText(user.getResidence_number());
         binding.etPhone.setText(user.getPhone());
+        if(user.getImagePath() != null) {
+            Picasso.get()
+                    .load(ApiConstant.SERVER_NAME + user.getImagePath())
+                    .fit()
+                    .into(binding.ivProfileImage);
+        }
+    }
+
+    @Override
+    public void updateProfileImageSuccess(String message, String path) {
+        makeToast(message);
+        if(path != null) {
+            Picasso.get().load(ApiConstant.SERVER_NAME + path).into(binding.ivProfileImage);
+            System.out.println("PATH: " + ApiConstant.SERVER_NAME + path);
+        }
     }
 
     private void makeToast(String message){
